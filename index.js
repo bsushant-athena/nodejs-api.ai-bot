d2cb09c598f544438474c95d2bd5d03cfdb99b3c
Sushant Borse <bsushant@athenahealth.com>
2017-11-04 10:53:43 +0530

    avinash changes for app

--- index.js
+++ index.js
@@ -1,80 +1,22 @@
-'use strict';
+var express = require('express')
+var randomItem = require('random-item');
+var app = express()
+var doctorList = ['John Snow', 'Sansa Stark', 'Tyrion Lannister', 'Arya Stark', 'White Walker'];
 
-const express = require('express');
-const bodyParser = require('body-parser');
+app.set('port', (process.env.PORT || 8000))
+app.use(express.static(__dirname + '/public'))
 
-const restService = express();
-
-restService.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-restService.use(bodyParser.json());
-
-restService.post('/chickenpox', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.temperature ? req.body.result.parameters.temperature : "Seems like some problem."
-    return res.json({
-        speech: speech
-    });
-});
-
-// restService.post('/slack-test', function(req, res) {
-
-//     var slack_message = {
-//         "text": "Details of JIRA board for Browse and Commerce",
-//         "attachments": [{
-//             "title": "JIRA Board",
-//             "title_link": "http://www.google.com",
-//             "color": "#36a64f",
-
-//             "fields": [{
-//                 "title": "Epic Count",
-//                 "value": "50",
-//                 "short": "false"
-//             }, {
-//                 "title": "Story Count",
-//                 "value": "40",
-//                 "short": "false"
-//             }],
-
-//             "thumb_url": "https://stiltsoft.com/blog/wp-content/uploads/2016/01/5.jira_.png"
-//         }, {
-//             "title": "Story status count",
-//             "title_link": "http://www.google.com",
-//             "color": "#f49e42",
-
-//             "fields": [{
-//                 "title": "Not started",
-//                 "value": "50",
-//                 "short": "false"
-//             }, {
-//                 "title": "Development",
-//                 "value": "40",
-//                 "short": "false"
-//             }, {
-//                 "title": "Development",
-//                 "value": "40",
-//                 "short": "false"
-//             }, {
-//                 "title": "Development",
-//                 "value": "40",
-//                 "short": "false"
-//             }]
-//         }]
-//     }
-//     return res.json({
-//         speech: "speech",
-//         displayText: "speech",
-//         source: 'webhook-echo-sample',
-//         data: {
-//             "slack": slack_message
-//         }
-//     });
-// });
+app.get('/', function(request, response) {
+  response.send('Practitioner Recommender Service!!')
+})
 
 
+app.get('/fetchdoctor', function(request, response) {
 
+    console.log("Fetching a doctor recommendation....")
+    response.send(randomItem(doctorList));
+})
 
-restService.listen((process.env.PORT || 8000), function() {
-    console.log("Server up and listening");
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
